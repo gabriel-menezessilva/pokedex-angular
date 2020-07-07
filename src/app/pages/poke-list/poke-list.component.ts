813d312da3d93478791190b40c46e3ae4fbc1778
Gabriel Felix Menezes da Silva <gabriel-menezessilva@hotmail.com>
2020-07-06 22:01:25 -0300

    adding sprites

--- src/app/pages/poke-list/poke-list.component.ts
+++ src/app/pages/poke-list/poke-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
 
-  pokemons = ['', '', '', '', '']
+  pokemons = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 
   constructor() { }
 
